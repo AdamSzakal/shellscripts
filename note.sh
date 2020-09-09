193652e61bb7ab6a5e2a75519c72f644183bd97c
adamszakal <adam.b.szakal@gmail.com>
2020-09-09 16:06:36 +0200

    initial commit

---
+++ note.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# save today's date with and without spaces
+date_full=$(date +%d\ %b\ %Y)
+date_compact=$(date +%d%b%Y)
+
+# 1. echo the header as well as the first argument into nvim's insert mode
+# 2. open a file named after today's date + the first argument
+echo i\# $date_full • $1 | nvim -s - $HOME/Dropbox/Notes/work/$date_compact_$1.md
