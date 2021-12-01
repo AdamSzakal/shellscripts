dfce651889cb4b626cbed0484ec5010ef800bff6
adamszakal <adam.b.szakal@gmail.com>
2021-12-01 13:37:13 +0100

    EOD

--- note.sh
+++ note.sh
@@ -6,6 +6,6 @@
 date_full=$(date +%d\ %b\ %Y)
 date_compact=$(date +%d%b%Y)
 
-# 1. echo the header as well as the first argument into nvim's insert mode
+# 1. echo the header as well as the first argument into vim's insert mode
 # 2. open a file named after today's date + the first argument
-echo i\# $date_full • $1 | nvim -s - $HOME/Dropbox/Notes/work/$date_compact_$1.md
+echo i\# $date_full • $1 | vim -s  $HOME/Dropbox/Notes/work/$date_compact_$1.md
